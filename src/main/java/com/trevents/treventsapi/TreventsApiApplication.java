528b380274ee5f0e5910ff306628d08d83671b0e
Arima_key <127151745+ArimaKey@users.noreply.github.com>
2023-12-06 10:51:56 -0500

    First commit

---
+++ src/main/java/com/trevents/treventsapi/TreventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.trevents.treventsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreventsApiApplication.class, args);
+	}
+
+}
